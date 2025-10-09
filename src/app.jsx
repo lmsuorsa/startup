4bcb045b4dc4c12e40244fcd4f5f0f6605eca5c3
Luke Suorsa <lmsuorsa@byu.edu>
2025-10-08 19:28:27 -0600

    routing set up

--- src/app.jsx
+++ src/app.jsx
@@ -2,38 +2,60 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Profile } from './profile/profile';
+import { Leaderboards } from './leaderboards/leaderboards';
+import { About } from './about/about';
+
 export default function App() {
     return (
-        <div className="app bg-dark text-light">
-            <header className="bg-dark text-light container-fluid">
-                <nav className="navbar fixed-top navbar-dark">
-                    <a className="navbar-brand" href="#">CrossWordPuzz</a>
-                    <menu className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" href="index.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="play.html">Play</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="profile.html">Profile</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="leaderboards.html">Leaderboards</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="about.html">About</a>
-                        </li>
-                    </menu>
-                </nav>
-            </header>
-            <main className="bg-light text-dark">App components</main>
-            <footer className="bg-dark text-white-50">
-                <div className="container-fluid">
-                    <span className="text-reset">Luke Suorsa</span>
-                    <a className="text-reset" href="https://github.com/lmsuorsa/startup">GitHub</a>
-                </div>
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div className="app bg-dark text-light">
+                <header className="bg-dark text-light container-fluid">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <a className="navbar-brand" href="#">CrossWordPuzz</a>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to=''>Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='play'>Play</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='profile'>Profile</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='leaderboards'>Leaderboards</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='about'>About</NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
+
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/play' element={<Play />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/leaderboards' element={<Leaderboards />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+                
+                <footer className="bg-dark text-white-50">
+                    <div className="container-fluid">
+                        <span className="text-reset">Luke Suorsa</span>
+                        <a className="text-reset" href="https://github.com/lmsuorsa/startup">GitHub</a>
+                    </div>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
