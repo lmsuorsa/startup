e76882a54e33e7388a0cbd54d3d5d8e832415097
Luke Suorsa <lmsuorsa@byu.edu>
2026-02-05 20:08:43 -0700

    checkpoint after app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,47 +4,32 @@ import './app.css';
 
 export default function App() {
   return (
-    <div className="app bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <div className="navbar-brand">
-            Simon<sup>&reg;</sup>
-          </div>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="play.html">
-                Play
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="scores.html">
-                Scores
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+  <div className="app bg-dark text-light">
+    <header className="container-fluid">
+      <nav className="navbar navbar-dark">
+        <a className="navbar-brand" href="#">Liar's Dice<sup>&reg;</sup></a>
+        <menu className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="index.html">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="play.html">Play</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="about.html">About</a>
+          </li>
+        </menu>
+      </nav>
+    </header>
 
-      <main>App components go here</main>
+    <main>App components go here</main>
 
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Author Name(s)</span>
-          <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
-            Source
-          </a>
-        </div>
-      </footer>
-    </div>
+    <footer className="bg-dark text-white-50">
+      <div className="container-fluid">
+        <span className="text-reset">Luke Suorsa</span>
+        <a className="text-reset" href="https://github.com/lmsuorsa/startup">Github</a>
+      </div>
+    </footer>
+  </div>
   );
 }
