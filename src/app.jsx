1d3042b81b474fc1c24f6b77a5dc9e44323cae44
Luke Suorsa <lmsuorsa@byu.edu>
2026-02-05 20:30:10 -0700

    routing functionality added

--- src/app.jsx
+++ src/app.jsx
@@ -2,34 +2,58 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { About } from './about/about';
+
 export default function App() {
   return (
-  <div className="app bg-dark text-light">
-    <header className="container-fluid">
-      <nav className="navbar navbar-dark">
-        <a className="navbar-brand" href="#">Liar's Dice<sup>&reg;</sup></a>
-        <menu className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="play.html">Play</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="about.html">About</a>
-          </li>
-        </menu>
-      </nav>
-    </header>
+    <BrowserRouter>
+      <div className="app bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+              Liar's Dice<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="play">
+                  Play
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="about">
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-    <main>App components go here</main>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/play' element={<Play />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-    <footer className="bg-dark text-white-50">
-      <div className="container-fluid">
-        <span className="text-reset">Luke Suorsa</span>
-        <a className="text-reset" href="https://github.com/lmsuorsa/startup">Github</a>
+        <footer className="bg-dark text-white-50">
+          <div className="container-fluid">
+            <span className="text-reset">Luke Suorsa</span>
+            <a className="text-reset" href="https://github.com/lmsuorsa/startup">Github</a>
+          </div>
+        </footer>
       </div>
-    </footer>
-  </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
