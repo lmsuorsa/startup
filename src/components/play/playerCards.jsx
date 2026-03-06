27308f44c40aeb501015ef829bc10ddbc69f4623
Luke Suorsa <lmsuorsa@byu.edu>
2026-03-06 16:07:06 -0700

    forgot to add playerCards.jsx

---
+++ src/components/play/playerCards.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Dice } from './dice.jsx'
+
+export function PlayerCard({ name, wins, previousBet, dice }) {
+  return (
+    <div className="player-card">
+      <div className="stat">
+        <span className="win-count">{wins}</span>
+        <span className="player-name">{name}</span>
+      </div>
+
+      <div className="stat">
+        <span className="previous-bet">
+          Previous Bet: {previousBet}
+        </span>
+      </div>
+
+      <div className="stat">
+        <div className="dice-container">
+          {dice.map((value, i) => (
+            <Dice key={i} value={value} hidden={value === null} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
