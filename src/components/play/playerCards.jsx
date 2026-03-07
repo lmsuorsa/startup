98a909098792c9607a82efacca2e569d000246e2
Luke Suorsa <lmsuorsa@byu.edu>
2026-03-06 17:14:41 -0700

    hidden parameter in player card

--- src/components/play/playerCards.jsx
+++ src/components/play/playerCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Dice } from './dice.jsx'
 
-export function PlayerCard({ name, wins, previousBet, dice }) {
+export function PlayerCard({ name, wins, previousBet, dice, hidden = false }) {
   return (
     <div className="player-card">
       <div className="stat">
@@ -19,7 +19,7 @@ export function PlayerCard({ name, wins, previousBet, dice }) {
       <div className="stat">
         <div className="dice-container">
           {dice.map((value, i) => (
-            <Dice key={i} value={value} hidden={value === null} />
+            <Dice key={i} value={value} hidden={hidden || value === null} />
           ))}
         </div>
       </div>
