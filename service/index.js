b62d9dfe6ec4a709c80895530a468695d58dd249
Luke Suorsa <lmsuorsa@byu.edu>
2026-03-11 23:00:00 -0600

    store users and wins in memory, service port, CreateAuth, GetAuth, DeleteAuth, verifyAuth

--- service/index.js
+++ service/index.js
@@ -4,3 +4,68 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
 
+// wins and users saved in memory, disappear when service restarted
+let users = [];
+let wins = [];
+
+// service port; front-end code statically hosted by service on same port
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// parse JSON body
+app.use(express.json());
+
+// track auth token cookies
+app.use(cookieParser());
+
+// serve front-end static content hosting
+app.use(express.static('public'));
+
+// router for service endpoints
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// verify user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
