788397ffd083bf7169a4d5d9f21b6bec8e963dfb
Luke Suorsa <lmsuorsa@byu.edu>
2026-03-12 20:29:27 -0600

    update auth functions

--- src/components/login/authenticated.jsx
+++ src/components/login/authenticated.jsx
@@ -9,8 +9,18 @@ export function Authenticated(props) {
   const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('userName');
-    props.onLogout();
+    // localStorage.removeItem('userName');
+    // props.onLogout();
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+      .catch(() => {
+        // Logout failed. Assuming offline
+      })
+      .finally(() => {
+        localStorage.removeItem('userName');
+        props.onLogout();
+      });
   }
 
   return (
