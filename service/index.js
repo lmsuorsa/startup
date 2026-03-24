b1d9eebe1689bb3b854aedec9b5bc504ec98d999
Luke Suorsa <lmsuorsa@byu.edu>
2026-03-24 15:32:53 -0600

    replace in-memory storage with db

--- service/index.js
+++ service/index.js
@@ -3,12 +3,9 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
-
-// wins and users saved in memory, disappear when service restarted
-let users = [];
-let wins = [];  // entries format: { name: string, wins: int }
 
 // service port; front-end code statically hosted by service on same port
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
